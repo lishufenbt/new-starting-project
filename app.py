cb7deabce636cefe742bf8c0befae00fc0d6db85
Yuantao Li <yl2642@nyu.edu>
2019-01-05 10:24:16 -0500

    a few changes

--- app.py
+++ app.py
@@ -37,6 +37,6 @@ def index():
 	return render_template('index.html')
 
 if __name__ == "__main__":
-	#port = int(os.environ.get("PORT", 5000))
-	#app.run(host='0.0.0.0', port=port)
-	app.run()
+	port = int(os.environ.get("PORT", 5000))
+	app.run(host='http://127.0.0.1', port=port)
+	#app.run()
